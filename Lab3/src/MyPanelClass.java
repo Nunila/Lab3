3ca6b28d15fa2358514e2e7e81f465d134db2a5c
nunila.davila <nunila.davila@INCADELWS11.ecenet.ece.uprm.edu>
2017-11-15 19:12:15 -0400

    Finished Puertorican flag. Created new light blue color.

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -22,12 +22,43 @@ public class MyPanelClass extends JPanel {
                         int y2 = getHeight() - myInsets.bottom - 1;
                         int width = x2 - x1;
                         int height = y2 - y1;
+                        Color starBlue = new Color (51, 153, 255);  
  
-                        //Paint the background
+                        //PUERTORICAN FLAG
+                        //Paint the background (for red lines)
                         g.setColor(Color.RED);
                         g.fillRect(x1, y1, width + 1, height + 1);
                         
-//                        //Draw a border
+                        //2 white lines
+                        g.setColor(Color.WHITE);
+                        g.fillRect(x1, y1+height/5, width+1, height/5);
+                        g.fillRect(x1,y1 +3* height/5, width+1, height/5);
+                        
+                        //Blue triangle                
+                        Polygon p3 = new Polygon();
+                        p3.addPoint(x1, y1);
+                        p3.addPoint(width/2, height/2);
+                        p3.addPoint(x1, y2);
+                        g.setColor(starBlue);
+                        g.fillPolygon(p3);
+                        
+                      //Star
+                        Polygon p2 = new Polygon();
+                        p2.addPoint(x1 + 25, y1 + 73);
+                        p2.addPoint(x1 + 41, y1 + 73);
+                        p2.addPoint(x1 + 47, y1 + 58);
+                        p2.addPoint(x1 + 53, y1 + 73);
+                        p2.addPoint(x1 + 69, y1 + 73);
+                        p2.addPoint(x1 + 56, y1 + 83);
+                        p2.addPoint(x1 + 61, y1 + 98);
+                        p2.addPoint(x1 + 47, y1 + 88);
+                        p2.addPoint(x1 + 34, y1 + 98);
+                        p2.addPoint(x1 + 38, y1 + 83);
+                        g.setColor(Color.WHITE); 
+                        g.fillPolygon(p2);
+                        
+                        
+                           //Draw a border
 //                        g.setColor(Color.RED);
 //                        g.drawRect(x1, y1, width, height);
 //                        g.setColor(Color.GREEN);
@@ -55,25 +86,6 @@ public class MyPanelClass extends JPanel {
 //                        g.setColor(Color.YELLOW);
 //                        g.fillPolygon(p);
                         
-                        //PUERTORICAN FLAG
-                        //Star
-                        Polygon p2 = new Polygon();
-                        p2.addPoint(x1 + 25, y1 + 73);
-                        p2.addPoint(x1 + 41, y1 + 73);
-                        p2.addPoint(x1 + 47, y1 + 58);
-                        p2.addPoint(x1 + 53, y1 + 73);
-                        p2.addPoint(x1 + 69, y1 + 73);
-                        p2.addPoint(x1 + 56, y1 + 83);
-                        p2.addPoint(x1 + 61, y1 + 98);
-                        p2.addPoint(x1 + 47, y1 + 88);
-                        p2.addPoint(x1 + 34, y1 + 98);
-                        p2.addPoint(x1 + 38, y1 + 83);
-                        g.setColor(Color.WHITE);
-                        g.fillPolygon(p2);
-                        
-                        //2 white lines
-                        g.setColor(Color.WHITE);
-                        g.fillRect(x1, y1+10, width, 10);
                         //g.setColor(Color.GREEN);
                         //g.drawRect(10, 10, width-20, height-20);
             }
